8071001d05bb8eb0a2f514e9989f7332b337e6d4
terppa90 <teromese@hotmail.com>
2026-04-16 14:52:32 +0300

    Next.js Basics/task2

---
+++ 2-Next.js_basics/task1/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <main>
+      <h1>About this course</h1>
+      <p>This is a Next.js fundamentals course from Metropolia</p>
+    </main>
+  );
+}
