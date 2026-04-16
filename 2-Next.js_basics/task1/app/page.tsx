fb4ff306e31429733ed7da5c98107ff43b3f3ee2
terppa90 <teromese@hotmail.com>
2026-04-16 15:46:59 +0300

    Next.js Basics/task5

--- 2-Next.js_basics/task1/app/page.tsx
+++ 2-Next.js_basics/task1/app/page.tsx
@@ -1,11 +1,12 @@
 'use client';
 import Link from 'next/link';
+import styles from './home.module.css';
 
 export default function Home() {
   return (
-    <main>
-      <h1>Fundamentals of Next.js</h1>
-      <p>This is an application for studying Next.js</p>
+    <main className={styles.container}>
+      <h1 className={styles.heading}>Fundamentals of Next.js</h1>
+      <p className={styles.description}>This is an application for studying Next.js</p>
       <Link href="/about" className="text-blue-500 hover:underline">
         Go to About Page
       </Link>
