abfe02208b802f5d325e4d311401de9db987ed7a
terppa90 <teromese@hotmail.com>
2026-04-16 15:19:48 +0300

    Next.js Basics/task3 & task4

--- 2-Next.js_basics/task1/app/about/page.tsx
+++ 2-Next.js_basics/task1/app/about/page.tsx
@@ -1,8 +1,14 @@
+'use client';
+import Link from 'next/link';
+
 export default function AboutPage() {
   return (
     <main>
-      <h1>About this course</h1>
-      <p>This is a Next.js fundamentals course from Metropolia</p>
+        <h1>About this course</h1>
+        <p>This is a Next.js fundamentals course from Metropolia</p>
+        <Link href="/" className="text-blue-500 hover:underline">
+          Back to Home
+        </Link>
     </main>
   );
 }
