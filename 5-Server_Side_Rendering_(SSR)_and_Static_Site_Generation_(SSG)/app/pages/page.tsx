ca706ba71c526940101266d9e1e4f0953892ba1f
terppa90 <teromese@hotmail.com>
2026-04-23 12:03:40 +0300

    Server_Side_Rendering_(SSR)_and_Static_Site_Generation_(SSG)/task2

--- 5-Server_Side_Rendering_(SSR)_and_Static_Site_Generation_(SSG)/app/pages/page.tsx
+++ 5-Server_Side_Rendering_(SSR)_and_Static_Site_Generation_(SSG)/app/pages/page.tsx
@@ -1,12 +1,20 @@
 import styles from '../home.module.css';
 
+export const revalidate = 60; //60 sek
+
 export default async function BlogPage() {
-    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+    next: { revalidate: 60 }
+  });
     const posts = await response.json();
+
+    const timestamp = new Date().toLocaleTimeString('fi-FI');
 
     return (
         <main style={{ padding: '20px', fontFamily: 'sans-serif' }}>
             <h1 className={styles.heading}>Blogipostaukset</h1>
+
+            <p style={{ color: 'gray' }}>Päivitetty viimeksi: {timestamp}</p>
 
             <ul>
             {posts.slice(0, 10).map((post) => (
