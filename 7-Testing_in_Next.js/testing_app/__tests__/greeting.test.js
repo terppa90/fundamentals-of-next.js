f0533091dfc39c15d1eb4cc853598bcfac738621
terppa90 <teromese@hotmail.com>
2026-04-24 09:08:51 +0300

    Testing_in_Next.js/task3

--- 7-Testing_in_Next.js/testing_app/__tests__/greeting.test.js
+++ 7-Testing_in_Next.js/testing_app/__tests__/greeting.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import Greeting from '../app/components/Greeting';
+import Greeting from '@/app/components/Greeting';
 
 test('renders greeting with name', () => {
   render(<Greeting name="Next.js" />);
